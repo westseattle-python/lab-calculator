e2048f8c802cb038ae548c3933535b906432a57c
teachspaces <teachspaces@gmail.com>
2016-07-08 23:29:28 -0700

    calculator skeleton code

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# Make a simple calculator that can add, subtract, multiply and divide using functions
+
+# define functions
+
+# take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+    pass
+elif choice == '2':
+    pass
+elif choice == '3':
+    pass
+elif choice == '4':
+    pass
+else:
+    pass
